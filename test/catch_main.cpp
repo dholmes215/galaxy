f1774acc793837cce0de9c020bde148b8005dc18
David Holmes <dholmes@dholmes.us>
2022-04-27 09:54:51 -0500

    Add Boost license

--- test/catch_main.cpp
+++ test/catch_main.cpp
@@ -1,3 +1,10 @@
+//
+// Copyright (c) 2022 David Holmes (dholmes at dholmes dot us)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #define CATCH_CONFIG_MAIN  // This tells the catch header to generate a main
 
 #include <catch2/catch.hpp>
