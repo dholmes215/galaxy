7e7f12a589494859b778985a31dac84a00b87303
David Holmes <dholmes@dholmes.us>
2022-04-28 18:47:30 -0500

    Refactor title_screen to work more like FTXUI code

--- src/game_ui.cpp
+++ src/game_ui.cpp
@@ -18,75 +18,72 @@ namespace galaxy {
 
 using namespace ftxui;
 
-class title_screen : public ftxui::ComponentBase {
-   public:
-    struct menu_callbacks {
-        std::function<void()> new_game;
-        std::function<void()> load_game;
-        std::function<void()> quit;
+struct title_callbacks {
+    std::function<void()> new_game;
+    std::function<void()> load_game;
+    std::function<void()> quit;
+};
+
+Component title_screen(title_callbacks callbacks)
+{
+    constexpr const int menu_width{20};
+    constexpr const int menu_height{5};
+    static const std::vector<std::string> menu_entries{"New Game", "Load Game",
+                                                       "Quit"};
+    class impl : public ftxui::ComponentBase {
+       public:
+        explicit impl(title_callbacks callbacks)
+            : callbacks_{std::move(callbacks)}
+        {
+            auto menu_action{[&] {
+                switch (menu_selected_) {
+                    case 0:
+                        callbacks_.new_game();
+                        break;
+                    case 1:
+                        callbacks_.load_game();
+                        break;
+                    case 2:
+                        callbacks_.quit();
+                        break;
+                    default:
+                        spdlog::info("Clicked Unknown");
+                        // TODO: Throw
+                        break;
+                }
+            }};
+            menu_option_.on_enter = menu_action;
+            Add(menu_);
+        }
+
+        ftxui::Element Render() override
+        {
+            return menu_->Render() | border | center |
+                   size(Direction::WIDTH, Constraint::EQUAL, menu_width) |
+                   size(Direction::HEIGHT, Constraint::EQUAL, menu_height) |
+                   align_right;
+        }
+
+       private:
+        title_callbacks callbacks_;
+        int menu_selected_{0};
+        ftxui::MenuOption menu_option_;
+        ftxui::Component menu_{
+            ftxui::Menu(&menu_entries, &menu_selected_, &menu_option_)};
     };
 
-    explicit title_screen(menu_callbacks callbacks)
-        : callbacks_{std::move(callbacks)}
-    {
-        auto menu_action{[&] {
-            switch (menu_selected_) {
-                case 0:
-                    callbacks_.new_game();
-                    break;
-                case 1:
-                    callbacks_.load_game();
-                    break;
-                case 2:
-                    callbacks_.quit();
-                    break;
-                default:
-                    spdlog::info("Clicked Unknown");
-                    // TODO: Throw
-                    break;
-            }
-        }};
-        menu_option_.on_enter = menu_action;
-        Add(menu_);
-    }
-
-    ftxui::Element Render() override
-    {
-        return menu_->Render() | border | center |
-               size(Direction::WIDTH, Constraint::EQUAL, menu_width_) |
-               size(Direction::HEIGHT, Constraint::EQUAL, menu_height_) |
-               align_right;
-    }
-
-   private:
-    static constexpr const int menu_width_{20};
-    static constexpr const int menu_height_{5};
-
-    menu_callbacks callbacks_;
-
-    static const std::vector<std::string>* menu_entries()
-    {
-        static const std::vector<std::string> out{"New Game", "Load Game",
-                                                  "Quit"};
-        return &out;
-    }
-
-    int menu_selected_{0};
-    ftxui::MenuOption menu_option_;
-    ftxui::Component menu_{
-        ftxui::Menu(menu_entries(), &menu_selected_, &menu_option_)};
-};
+    return Make<impl>(std::move(callbacks));
+}
 
 void run_game(const std::vector<ftxui::Event>& events)
 {
     auto screen{ftxui::ScreenInteractive::Fullscreen()};
 
-    title_screen::menu_callbacks callbacks{
+    title_callbacks callbacks{
         .new_game = [] { spdlog::info("Clicked New Game"); },
         .load_game = [] { spdlog::info("Clicked Load Game"); },
         .quit = screen.ExitLoopClosure()};
-    ftxui::Component component{
-        std::make_shared<title_screen>(std::move(callbacks))};
+    ftxui::Component component{title_screen(std::move(callbacks))};
 
     // Inject events for test automation
     for (const auto& event : events) {
